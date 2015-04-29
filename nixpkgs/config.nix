4f5a5fc74f3105ce5ac25ffa44fc795ce8440bab
C0DEHERO <codehero@nerdpol.ch>
2015-04-29 17:37:15 -0400

    added curl package to haskell

--- nixpkgs/config.nix
+++ nixpkgs/config.nix
@@ -4,7 +4,7 @@
   packageOverrides = super: let self = super.pkgs; in
   {
     hsEnv = self.haskellngPackages.ghcWithPackages (p: with p; [
-      cabal-install cabal2nix lens text transformers aeson ansi-terminal sqlite-simple system-filepath
+      cabal-install cabal2nix lens text transformers aeson ansi-terminal sqlite-simple system-filepath curl
     ]);
   };
 }
