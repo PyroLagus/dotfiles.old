eda65653e76318ed5e5213ffc6151128509e70b5
C0DEHERO <codehero@nerdpol.ch>
2015-04-29 17:43:18 -0400

    added firefox wrapper to per-user config.nix

--- nixpkgs/config.nix
+++ nixpkgs/config.nix
@@ -1,10 +1,13 @@
 # ~/.nixpkgs/config.nix
 
 {
+  allowUnfree = true;
+
   packageOverrides = super: let self = super.pkgs; in
   {
     hsEnv = self.haskellngPackages.ghcWithPackages (p: with p; [
       cabal-install cabal2nix lens text transformers aeson ansi-terminal sqlite-simple system-filepath curl
     ]);
+    firefox-bin-wrapper = pkgs.wrapFirefox { browser = pkgs.firefox-bin; };
   };
 }
